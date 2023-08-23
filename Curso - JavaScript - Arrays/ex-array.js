ef3af742a607af4ac4ac4835f553bc04e9c672f8
Beatriz Lirios <beatriz.lirios@anota.ai>
2023-08-23 10:41:35 -0300

    Curso JavaScript Alura

---
+++ Curso - JavaScript - Arrays/ex-array.js
@@ -0,0 +1,21 @@
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array
+const numeros = [50, 43, 12];
+
+//Array 
+//Um array pode ser definido como uma lista ordenada de valores enumerados em que cada valor é chamado de elemento ou item, e cada elemento se localiza numa posição na lista chamada de índice. Vamos destrinchar essa explicação.
+
+//Cada valor é chamado de elemento ou item:
+
+// array com 1 elemento
+const numero = [5];
+
+// array com 3 elementos
+const nums = [50, 43, 12];
+
+//Cada elemento está localizado em uma posição na lista, chamada de índice:
+const nums = [50, 43, 12];
+// o número 43 está na posição de número 1 (segunda posição) nessa lista de três números.
